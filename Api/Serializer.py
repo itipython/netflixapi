6ca9cf1efec6f3f235d8941180e2b896edc15b20
mwadie <minawadie19@gmail.com>
2021-04-14 20:24:31 +0200

    fix serializers

--- Api/Serializer.py
+++ Api/Serializer.py
@@ -80,5 +80,5 @@ class WatchLaterSerializer(serializers.ModelSerializer):
 class WatchedSerializer(serializers.ModelSerializer):
     class Meta:
         model = Watched
-        fields =['show_id','user_id']
+        fields =['Show_id','User_id']
 
